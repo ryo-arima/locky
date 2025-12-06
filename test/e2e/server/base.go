d4b1b5977954d6404445f372eaae55213adf2851
Ryo ARIMA <ryo.arima@zohomail.jp>
2025-12-06 22:40:02 +0900

    refactor: Remove logger initialization from StartTestServer function

--- test/e2e/server/base.go
+++ test/e2e/server/base.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/ryo-arima/locky/pkg/config"
 	"github.com/ryo-arima/locky/pkg/entity/model"
-	"github.com/ryo-arima/locky/pkg/logger"
 	"github.com/ryo-arima/locky/pkg/server"
 	"github.com/ryo-arima/locky/pkg/server/share"
 )
@@ -47,9 +46,6 @@ func StartTestServer() error {
 	config.SetLoggerFactory(share.NewLogger)
 
 	conf := config.NewBaseConfig()
-
-	// Initialize global logger
-	logger.Initialize(conf.YamlConfig.Logger)
 
 	if err := conf.ConnectDB(); err != nil {
 		return fmt.Errorf("failed to connect DB: %w", err)
